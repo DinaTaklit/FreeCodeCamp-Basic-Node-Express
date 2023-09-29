1b3b515f015edc22e7dfbccbeba78e3aeb1bb555
Taklit Dina <ez_taklit@esi.dz>
2023-09-29 19:38:43 +0100

    feat: Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 
 console.log("Hello World");
 
 app.use("/public", express.static(`${__dirname}/public`));
 
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
 
 app.get("/", (req, res) => {
